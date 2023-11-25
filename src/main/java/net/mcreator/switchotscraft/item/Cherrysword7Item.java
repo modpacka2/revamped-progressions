162f78033a2bacf40c9db9b5e2d406c77e8a0adc
modpacka2 <75133238+modpacka2@users.noreply.github.com>
2023-11-25 13:01:47 -0500

    -wooden swords and axes

---
+++ src/main/java/net/mcreator/switchotscraft/item/Cherrysword7Item.java
@@ -0,0 +1,71 @@
+
+package net.mcreator.switchotscraft.item;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+
+import net.mcreator.switchotscraft.init.SwitchotsCraftModItems;
+
+import java.util.List;
+
+public class Cherrysword7Item extends SwordItem {
+	public Cherrysword7Item() {
+		super(new Tier() {
+			public int getUses() {
+				return 59;
+			}
+
+			public float getSpeed() {
+				return 2f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 2f;
+			}
+
+			public int getLevel() {
+				return 0;
+			}
+
+			public int getEnchantmentValue() {
+				return 15;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of(new ItemStack(SwitchotsCraftModItems.FIBESR_STRING.get()));
+			}
+		}, 3, -2.4f, new Item.Properties());
+	}
+
+	@Override
+	public boolean hasCraftingRemainingItem(ItemStack stack) {
+		return true;
+	}
+
+	@Override
+	public ItemStack getCraftingRemainingItem(ItemStack itemstack) {
+		ItemStack retval = new ItemStack(this);
+		retval.setDamageValue(itemstack.getDamageValue() + 1);
+		if (retval.getDamageValue() >= retval.getMaxDamage()) {
+			return ItemStack.EMPTY;
+		}
+		return retval;
+	}
+
+	@Override
+	public boolean isRepairable(ItemStack itemstack) {
+		return false;
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
+		list.add(Component.literal("stick made in : mangrove"));
+	}
+}
--- src/main/java/net/mcreator/switchotscraft/procedures/AxecuttingProcedure.java
+++ src/main/java/net/mcreator/switchotscraft/procedures/AxecuttingProcedure.java
@@ -7,15 +7,14 @@ import net.minecraftforge.event.level.BlockEvent;
 
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
-
-import net.mcreator.switchotscraft.init.SwitchotsCraftModItems;
 
 import javax.annotation.Nullable;
 
@@ -33,12 +32,7 @@ public class AxecuttingProcedure {
 	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
-		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == Items.WOODEN_AXE || (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == Items.STONE_AXE
-				|| (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == Items.IRON_AXE
-				|| (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == Items.GOLDEN_AXE
-				|| (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == Items.NETHERITE_AXE
-				|| (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == SwitchotsCraftModItems.BASIC_AXE.get()
-				|| (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == Items.DIAMOND_AXE) {
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).is(ItemTags.create(new ResourceLocation("forge:axes")))) {
 			if ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == Blocks.OAK_LOG) {
 				if (world instanceof ServerLevel _level) {
 					ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(Blocks.STRIPPED_OAK_LOG));
