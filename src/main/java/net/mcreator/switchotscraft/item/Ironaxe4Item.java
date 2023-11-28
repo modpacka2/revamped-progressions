2836d22cdc09fbc6d43a90cdb66f8e8b2a7a5fa0
modpacka2 <75133238+modpacka2@users.noreply.github.com>
2023-11-28 09:48:07 -0500

    -iron tools

---
+++ src/main/java/net/mcreator/switchotscraft/item/Ironaxe4Item.java
@@ -0,0 +1,72 @@
+
+package net.mcreator.switchotscraft.item;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.network.chat.Component;
+
+import net.mcreator.switchotscraft.init.SwitchotsCraftModItems;
+
+import java.util.List;
+
+public class Ironaxe4Item extends AxeItem {
+	public Ironaxe4Item() {
+		super(new Tier() {
+			public int getUses() {
+				return 125;
+			}
+
+			public float getSpeed() {
+				return 6f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 7f;
+			}
+
+			public int getLevel() {
+				return 2;
+			}
+
+			public int getEnchantmentValue() {
+				return 14;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of(new ItemStack(SwitchotsCraftModItems.FIBESR_STRING.get()));
+			}
+		}, 1, -3.1f, new Item.Properties());
+	}
+
+	@Override
+	public boolean hasCraftingRemainingItem(ItemStack stack) {
+		return true;
+	}
+
+	@Override
+	public ItemStack getCraftingRemainingItem(ItemStack itemstack) {
+		ItemStack retval = new ItemStack(this);
+		retval.setDamageValue(itemstack.getDamageValue() + 1);
+		if (retval.getDamageValue() >= retval.getMaxDamage()) {
+			return ItemStack.EMPTY;
+		}
+		return retval;
+	}
+
+	@Override
+	public boolean isRepairable(ItemStack itemstack) {
+		return false;
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
+		list.add(Component.literal("stick made in : jungle"));
+		list.add(Component.literal("string made in : fiber string"));
+	}
+}
---
+++ src/main/java/net/mcreator/switchotscraft/item/Ironswordstring3Item.java
@@ -0,0 +1,71 @@
+
+package net.mcreator.switchotscraft.item;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+
+import java.util.List;
+
+public class Ironswordstring3Item extends SwordItem {
+	public Ironswordstring3Item() {
+		super(new Tier() {
+			public int getUses() {
+				return 375;
+			}
+
+			public float getSpeed() {
+				return 6f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 2f;
+			}
+
+			public int getLevel() {
+				return 2;
+			}
+
+			public int getEnchantmentValue() {
+				return 14;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of(new ItemStack(Items.STRING));
+			}
+		}, 3, -2.4f, new Item.Properties());
+	}
+
+	@Override
+	public boolean hasCraftingRemainingItem(ItemStack stack) {
+		return true;
+	}
+
+	@Override
+	public ItemStack getCraftingRemainingItem(ItemStack itemstack) {
+		ItemStack retval = new ItemStack(this);
+		retval.setDamageValue(itemstack.getDamageValue() + 1);
+		if (retval.getDamageValue() >= retval.getMaxDamage()) {
+			return ItemStack.EMPTY;
+		}
+		return retval;
+	}
+
+	@Override
+	public boolean isRepairable(ItemStack itemstack) {
+		return false;
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
+		list.add(Component.literal("stick made in : birch"));
+		list.add(Component.literal("string made in : spider string"));
+	}
+}
