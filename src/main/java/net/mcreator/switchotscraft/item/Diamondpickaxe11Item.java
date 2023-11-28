a7c16d3b7b7310941b4adad84a77e286872dc7e9
modpacka2 <75133238+modpacka2@users.noreply.github.com>
2023-11-28 17:07:54 -0500

    -finish basics of update

---
+++ src/main/java/net/mcreator/switchotscraft/item/Diamondpickaxe11Item.java
@@ -0,0 +1,72 @@
+
+package net.mcreator.switchotscraft.item;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+
+import net.mcreator.switchotscraft.init.SwitchotsCraftModItems;
+
+import java.util.List;
+
+public class Diamondpickaxe11Item extends PickaxeItem {
+	public Diamondpickaxe11Item() {
+		super(new Tier() {
+			public int getUses() {
+				return 781;
+			}
+
+			public float getSpeed() {
+				return 8f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 3f;
+			}
+
+			public int getLevel() {
+				return 3;
+			}
+
+			public int getEnchantmentValue() {
+				return 10;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of(new ItemStack(SwitchotsCraftModItems.FIBESR_STRING.get()));
+			}
+		}, 1, -2.8f, new Item.Properties());
+	}
+
+	@Override
+	public boolean hasCraftingRemainingItem(ItemStack stack) {
+		return true;
+	}
+
+	@Override
+	public ItemStack getCraftingRemainingItem(ItemStack itemstack) {
+		ItemStack retval = new ItemStack(this);
+		retval.setDamageValue(itemstack.getDamageValue() + 1);
+		if (retval.getDamageValue() >= retval.getMaxDamage()) {
+			return ItemStack.EMPTY;
+		}
+		return retval;
+	}
+
+	@Override
+	public boolean isRepairable(ItemStack itemstack) {
+		return false;
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
+		list.add(Component.literal("stick made in : bamboo"));
+		list.add(Component.literal("string made in : fiber string"));
+	}
+}
---
+++ src/main/java/net/mcreator/switchotscraft/item/Diamondshovel2Item.java
@@ -0,0 +1,72 @@
+
+package net.mcreator.switchotscraft.item;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.ShovelItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+
+import net.mcreator.switchotscraft.init.SwitchotsCraftModItems;
+
+import java.util.List;
+
+public class Diamondshovel2Item extends ShovelItem {
+	public Diamondshovel2Item() {
+		super(new Tier() {
+			public int getUses() {
+				return 781;
+			}
+
+			public float getSpeed() {
+				return 8f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 3.5f;
+			}
+
+			public int getLevel() {
+				return 3;
+			}
+
+			public int getEnchantmentValue() {
+				return 10;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of(new ItemStack(SwitchotsCraftModItems.FIBESR_STRING.get()));
+			}
+		}, 1, -3f, new Item.Properties());
+	}
+
+	@Override
+	public boolean hasCraftingRemainingItem(ItemStack stack) {
+		return true;
+	}
+
+	@Override
+	public ItemStack getCraftingRemainingItem(ItemStack itemstack) {
+		ItemStack retval = new ItemStack(this);
+		retval.setDamageValue(itemstack.getDamageValue() + 1);
+		if (retval.getDamageValue() >= retval.getMaxDamage()) {
+			return ItemStack.EMPTY;
+		}
+		return retval;
+	}
+
+	@Override
+	public boolean isRepairable(ItemStack itemstack) {
+		return false;
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
+		list.add(Component.literal("stick made in : spruce"));
+		list.add(Component.literal("string made in : fiber string"));
+	}
+}
